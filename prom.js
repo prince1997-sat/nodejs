37af401a53e345b978c40a36508953c98e2e09d3
prince1997-sat <82972981+prince1997-sat@users.noreply.github.com>
2022-07-28 07:53:20 +0530

    Add files via upload

---
+++ prom.js
@@ -0,0 +1,13 @@
+function prince(a, b) {
+    return a * b;
+}
+var pro = new Promise(function(resolve, reject) {
+    request.get(options, function(err, resp, body) {
+        if (err) {
+            reject(err);
+
+        } else {
+            resolve();
+        }
+    })
+})
